4a8c05c56950954dbd3a300b83d975d57945bbb3
TheReasonWePlay <randrian.william@gmail.com>
2026-09-19 12:00:22 +0300

    commit initiale

---
+++ src/main/java/com/javaAV/coivoiturage/CoivoiturageApplication.java
@@ -0,0 +1,13 @@
+package com.javaAV.coivoiturage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoivoiturageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoivoiturageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaAV/coivoiturage/CoivoiturageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaAV.coivoiturage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoivoiturageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
